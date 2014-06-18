57750d2f632c3140eb131d3c182e2161c313d06b
Kent Fredric <kentfredric@gmail.com>
2014-06-19 10:13:08 +1200

    Downgrade is not an upgrade

--- lib/CPAN/Meta/Prereqs/Diff/Downgrade.pm
+++ lib/CPAN/Meta/Prereqs/Diff/Downgrade.pm
@@ -15,7 +15,7 @@ use Moo qw( with has );
 
 extends 'CPAN::Meta::Prereqs::Diff::Change';
 
-sub is_upgrade { 1 }
+sub is_downgrade { 1 }
 
 sub describe {
   return sprintf q[%s.%s: vvv %s %s -> %s], $_[0]->phase, $_[0]->type, $_[0]->module, $_[0]->old_requirement,
